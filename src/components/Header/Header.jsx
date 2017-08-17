a57c8abffb2b5a3ae0de861b5becf9ab6165d33c
Harish Mohan Raj <hraj@sapient.com>
2017-08-17 16:21:11 +0100

    private components are created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Navigation from '../Navigation/Navigation.jsx';
 import './Header.css';
 
-class Header extends Component {
-  render() {
-    const navItems = this.props.navItems;
-    return (
-      <div className="App-header">
-        <img src={this.props.headerLogo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-        <Navigation navItems={navItems}/>
-      </div>
-    )
-  }
+const Header = ({headerLogo, navItems}) => {
+  return (
+    <div className="App-header">
+      <img src={headerLogo} className="App-logo" alt="logo" />
+      <h2>Welcome to React</h2>
+      <Navigation navItems={navItems}/>
+    </div>
+  )
 }
 
 export default Header;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,65 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Navigation.css';
 
+// ListGenerator Component
+class ListGenerator extends Component {
+  render() {
+    return(
+      <li
+        className = {this.props.item === this.props.selectedItem ? 'item selected': 'item'}
+        onClick={this.props.clickHandler.bind(null, this.props.item)}
+        >
+          {this.props.item}
+      </li>
+    )
+  }
+}
+
+ListGenerator.PropTypes = {
+  item: PropTypes.string.isRequired,
+  selectedItem: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired
+}
+
+ListGenerator.defaultProps = {
+  item: 'Home',
+  selectedItem: 'Home',
+  clickHandler: () => {}
+}
+
+// NavItemsGenerator Component
+class NavItemsGenerator extends Component {
+  render() {
+    return (
+      <ul className='nav-list'>
+        {this.props.navItems.map(item => {
+          return (
+            <ListGenerator
+              item={item}
+              selectedItem={this.props.selectedItem}
+              clickHandler = {this.props.clickHandler}
+              key = {item}
+            />
+          )
+        })}
+      </ul>
+    )
+  }
+}
+
+NavItemsGenerator.PropTypes = {
+  navItems: PropTypes.array.isRequired,
+  selectedItem: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired
+}
+
+NavItemsGenerator.defaultProps = {
+  navItems: ['Default Prop'],
+  selectedItem: 'Home',
+  clickHandler: () => {}
+}
+
+// Navigation Component
 class Navigation extends Component {
   constructor(props) {
     super(props);
@@ -19,17 +78,11 @@ class Navigation extends Component {
 
   render() {
     return (
-      <ul className='nav-list'>
-      {this.props.navItems.map(item => {
-        return (<li
-        key={item}
-        className = {item === this.state.selectedItem ? 'item selected': 'item'}
-        onClick={this.clickHandler.bind(null, item)}
-        >
-          {item}
-        </li>)
-      })}
-      </ul>
+      <NavItemsGenerator
+        navItems = {this.props.navItems}
+        selectedItem = {this.state.selectedItem}
+        clickHandler = {this.clickHandler}
+      />
     )
   }
 }
