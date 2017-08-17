f415c595c635e8426a89797856c53c732f6c0221
Harish Mohan Raj <hraj@sapient.com>
2017-08-17 12:58:13 +0100

    props assigned to a variable

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,12 @@ import './Header.css';
 
 class Header extends Component {
   render() {
+    const navItems = this.props.navItems;
     return (
       <div className="App-header">
         <img src={this.props.headerLogo} className="App-logo" alt="logo" />
         <h2>Welcome to React</h2>
-        <Navigation navItems={this.props.navItems}/>
+        <Navigation navItems={navItems}/>
       </div>
     )
   }
