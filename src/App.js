bb674c3114cba10f71903eeaad8752d4caf952f9
Harish Mohan Raj <hraj@sapient.com>
2017-08-17 12:08:48 +0100

    logo src passed via props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import Header from './components/Header/Header.jsx';
 import './App.css';
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header headerLogo={logo} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
