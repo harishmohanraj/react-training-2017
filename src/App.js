3d337cb607337dc6b928ccfea0eb5e48cded8d91
Harish Mohan Raj <hraj@sapient.com>
2017-08-20 08:11:43 +0100

    data fetching completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
 import Header from './components/Header/Header.jsx';
+import UserDetails from './components/UserDetails/UserDetails.jsx'
+import fetchData from './utils/service';
 import appData from './app-data/appData.json';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      userData: null
+    }
+  }
+  componentDidMount() {
+    const url = 'https://jsonplaceholder.typicode.com/users'
+    fetchData(url)
+      .then((res) => {
+        const userData = res.data;
+        this.setState({
+          loading: false,
+          userData: userData
+        })
+      })
+      .catch(e => console.log(e))
+  }
   render() {
     const navItems = appData.navItems;
     return (
       <div className="App">
         <Header headerLogo={logo} navItems={navItems}/>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.loading ? <p>Loading...</p> : <UserDetails userData={this.state.userData}/>}
       </div>
     );
   }
